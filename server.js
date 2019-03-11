8f8153a6fd46608be82745661e465373d39ba92e
Nika Sepiskveradze <sebiskver@gmail.com>
2019-03-11 22:05:26 +0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello AshWood!");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening http://localhost:${port}`));
