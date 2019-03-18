c822b85a99ad27f803e64dd18784d4922ab17cfc
Nika Sepiskveradze <sebiskver@gmail.com>
2019-03-18 20:38:59 +0400

    Footer Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,46 @@ import React, { Component } from "react";
 class Footer extends Component {
   state = {};
   render() {
-    return <h1>Footer changed</h1>;
+    return (
+      <React.Fragment>
+        <footer id="footer-section" className="text-center">
+          <div className="container">
+            <div className="row upper-footer">
+              <div className="col-md-4 pb-4">
+                <h6>LOCATION</h6>
+                <p className="text-secondary w-50 m-auto">
+                  2313 Melcrose Place Beverly Hills, CA 90012
+                </p>
+              </div>
+              <div className="col-md-4 pb-4">
+                <h6 className="">SHARE WITH LOVE</h6>
+                <a href="/">
+                  <span className="fa fa-facebook" />
+                </a>
+                <a href="/">
+                  <span className="fa fa-google" />
+                </a>
+                <a href="/">
+                  <span className="fa fa-instagram" />
+                </a>
+              </div>
+              <div className="col-md-4 pb-4">
+                <h6>Ashwood University</h6>
+                <p className="text-secondary m-auto">
+                  Ashwood University's online shop, choose your university
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+        <div className="below-footer py-3 text-center">
+          <p className="text-muted">
+            Copyright &copy; 2019. All Right Reserved
+          </p>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
