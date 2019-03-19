f10d979849f0db47e5cb2503762ad994bf873cfd
Nika Sepiskveradze <sebiskver@gmail.com>
2019-03-20 02:08:28 +0400

    User Rendering Complete

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import * as profileService from "../services/profileService";
-import About from "./About";
 
 class Profile extends Component {
-  state = {};
+  state = {
+    user: {
+      name: "",
+      email: "",
+      age: "",
+      birthday: "",
+      balance: ""
+    }
+  };
 
   async componentDidMount() {
     const { data: user } = await profileService.getUserInfo();
-
     console.log(user);
+    this.setState({ user });
   }
 
   render() {
+    const { name, email, age, birthday, balance } = this.state.user;
+
     return (
       <div id="profile" className="py-4 bg-light">
         <div className="container">
@@ -33,28 +42,28 @@ class Profile extends Component {
           <div className="tab-content">
             <div id="about" className="tab-pane fade show active">
               <div className="row mt-3">
-                <div className="col-md-2">Name, Lastname</div>
-                <div className="col-md-4">Nika Sepiskveradze</div>
+                <div className="col-md-2">Name</div>
+                <div className="col-md-4">{name}</div>
               </div>
 
               <div className="row mt-3">
                 <div className="col-md-2">Email</div>
-                <div className="col-md-4">sebiskver@gmail.com</div>
+                <div className="col-md-4">{email}</div>
               </div>
 
               <div className="row mt-3">
                 <div className="col-md-2">Age</div>
-                <div className="col-md-4">21</div>
+                <div className="col-md-4">{age}</div>
               </div>
 
               <div className="row mt-3">
                 <div className="col-md-2">Birthday</div>
-                <div className="col-md-4">1997/12/21</div>
+                <div className="col-md-4">{birthday}</div>
               </div>
 
               <div className="row mt-3">
                 <div className="col-md-2">Balance</div>
-                <div className="col-md-4">100$</div>
+                <div className="col-md-4">{balance}$</div>
               </div>
             </div>
 
